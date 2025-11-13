a5751e1bb714146b5773601042193e611909bc78
Karthikeya1610 <karthikeya@zaktis.com>
2025-11-14 00:18:26 +0530

    updated the changes in the port running

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python3
-"""
-Simple Python script to run FastAPI app
-Usage: python run.py
-"""
 
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render gives the correct port
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=port
     )
-
